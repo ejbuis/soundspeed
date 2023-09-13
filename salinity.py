d916e4da1d0ce1d88083d8b1b09e56210cd21fe4
Ernst-Jan Buis <ejbuis@users.noreply.github.com>
2023-09-13 15:32:35 +0200

    Update salinity.py

--- salinity.py
+++ salinity.py
@@ -72,7 +72,7 @@ def main():
     aConductivity = 40# 1.888091
     Temp = 40
     Press = 10000
-    print Cond2Sal78(aConductivity, Temp, Press)
+    print(Cond2Sal78(aConductivity, Temp, Press))
     
     
 if __name__ == "__main__":
